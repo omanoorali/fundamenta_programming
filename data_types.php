e02736106e0fcabb226434e7d2354ac70c5a4495
omansoor <mansoor.ali@softnationtechnologies.com>
2026-05-11 02:28:32 -0700

    learnig basic programming fundamentals

---
+++ data_types.php
@@ -0,0 +1,99 @@
+<?php
+
+// echo "testing page";
+
+// boolean return true/false or 0/1
+// $is_admin = true;
+// if($is_admin){
+//     echo "you can acess deshboard";
+// }else{
+//     echo "your can not access deshboard ";
+// }
+// $data = ['service','products'];
+// echo $data[0];
+
+// echo "testing page";
+
+// boolean return true/false or 0/1
+// $is_admin = true;
+// if($is_admin){
+//     echo "you can acess deshboard";
+// }else{
+//     echo "your can not access deshboard ";
+// }
+    // array
+// $data = ['service','products'];
+// echo $data[0];
+    //object
+
+//    $user = [
+//     "name"=>"test",
+//       "age"=>23,
+//       "city"=>"karachi",
+//    ];
+
+//    $data = (object) $user;
+
+//    echo $data->city;
+//    echo $data->age;
+
+
+// calculator
+
+// $num1 = 2;
+// $num2 = 4;
+
+// echo $num1 + $num2;
+// echo "<br>";
+// echo $num1 - $num2;
+// echo "<br>";
+// echo $num1 / $num2;
+// echo "<br>";
+// echo $num1 * $num2;
+
+// Temprature caluculator.
+
+// $calius = 30;
+// $temp = ($calius * 9/5) + 32;
+// echo $temp;
+
+// name converter // uper and lowe case.
+echo "dasdf";
+// echo $name = "ali";
+// $last_name = "khan";
+
+// $full_name = strtoupper($name)." ". strtolower($last_name);
+// echo $full_name;
+
+// echo "testing page";
+
+// name converter // uper and lowe case.
+// echo "dasdf";
+//  $name = "ali";
+// $last_name = "KHAN";
+
+// $full_name = strtoupper($name)." ". strtolower($last_name);
+// echo $full_name;
+
+
+// REAL word pracice 
+// task 1  
+// 5 object and 5 arrays
+
+// 1 object
+
+$user =[
+    "name"=>"khan",
+    "email"=>"khan@gamil.com",
+    "is_admin"=>true,
+    "age"=>22,
+    "skills"=>"php laravel"
+];
+
+$data = (object) $user;
+echo $data.email;
+
+
+// Task2 Product Array
+
+?> 
